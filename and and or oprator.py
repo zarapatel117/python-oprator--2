39262d9e493c039e9c78bbf8aeddc3cdf9f0b469
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-14 14:55:22 +0530

    and and or

---
+++ and and or oprator.py
@@ -0,0 +1,14 @@
+NUM1= 2
+num2 = -5
+num3 = 7
+if NUM1 and num2 and num3:
+    print ("all the num have boolean value as true")
+
+else:
+    print ("num have boolean value as false")  
+
+if NUM1>0 or num2>0 or num3>0:
+    print (" num are greater than 0")   
+
+else :
+    print ("0 is greater than num")
