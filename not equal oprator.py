a65959809a2875d381c339476d3659419867e19e
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-14 15:13:55 +0530

    not equal

---
+++ not equal oprator.py
@@ -0,0 +1,24 @@
+num1=4
+num2=9
+num3=4
+if num1!=num2:
+    print("num1 and num2 is not equal")
+
+else :
+    print ("num1 and num2 are equal")   
+
+if num1!= num3:     
+    print(" num1 and num2 are also not equal")
+
+else:
+    print("But num1 and num3 are equal")   
+
+X=int(input("Enter a num"))
+if X%2!=0:
+    print( "is not even num",X)
+
+else:
+    print(" is even num",X)    
+
+
+  
